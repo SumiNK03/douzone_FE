135eccd68d7cf3dd5725804e4a7cb1b6123b7f6a
SumiNK03 <0417nam@naver.com>
2025-11-27 00:20:55 +0900

    해시라우터로 변경

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/douzone_FE">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
