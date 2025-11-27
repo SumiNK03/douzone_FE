19ace560f59206b8308f34ffebe856dc4f9278ef
SumiNK03 <0417nam@naver.com>
2025-11-27 18:43:35 +0900

    로컬 호스팅 위한 url 변경

--- src/config/const.js
+++ src/config/const.js
@@ -1,3 +1,4 @@
 // src/config/const.js
-// export const API_URL = "http://localhost:8080";
-export const API_URL= "http://168.107.38.70:8080";
+export const API_URL = "http://localhost:8080";
+// export const API_URL= "https://168.107.38.70:8080";
+// export const API_URL = "http://douzonesumin.kro.kr:8080"
