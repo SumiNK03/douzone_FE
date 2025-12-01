054b1acfe0fd40f58cc870a7da06b44422a98107
SumiNK03 <0417nam@naver.com>
2025-12-01 22:32:21 +0900

    회원가입 반응형 웹

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -14,16 +14,21 @@ export default function SignupPage() {
                     <h1 className="font-bold text-4xl text-gray-700 mb-10">
                         회원가입 사용자 유형
                     </h1>
-                    <div className="flex gap-64 mt-[50px]">
+                    <div className="flex flex-wrap gap-6 mt-12 justify-center sm:gap-64 sm:flex-nowrap">
                         <button
                             onClick={() => setRole("teacher")}
-                            className="rounded-full bg-[#4DBAE7] text-white text-4xl px-14 py-14 shadow-lg hover:bg-[#32A4D3] transition w-[20vw] h-[20vw] font-bold"
+                            className="flex items-center justify-center text-white font-bold rounded-full 
+               bg-[#4DBAE7] hover:bg-[#32A4D3] shadow-lg 
+               w-40 h-40 text-xl text-center sm:w-[20vw] sm:h-[20vw] sm:text-4xl transition"
                         >
                             강사
                         </button>
+
                         <button
                             onClick={() => setRole("student")}
-                            className="rounded-full bg-[#4DBAE7] text-white text-4xl px-14 py-14 shadow-lg hover:bg-[#32A4D3] transition w-[20vw] h-[20vw] font-bold"
+                            className="flex items-center justify-center text-white font-bold rounded-full 
+               bg-[#4DBAE7] hover:bg-[#32A4D3] shadow-lg 
+               w-40 h-40 text-xl text-center sm:w-[20vw] sm:h-[20vw] sm:text-4xl transition"
                         >
                             학생
                         </button>
@@ -32,7 +37,7 @@ export default function SignupPage() {
             ) : (
                 <div className="w-[80vw] max-w-[700px] bg-white p-8 rounded-2xl shadow-md mt-6">
                     <div className="flex justify-between mb-6">
-                        <h2 className="text-3xl font-semibold text-gray-700">
+                        <h2 className="text-xl sm:text-3xl font-semibold text-gray-700">
                             {role === "teacher" ? "강사 회원가입" : "학생 회원가입"}
                         </h2>
                         <button
